dca3f8087b163a936d858d39a2b27b339651c071
olgnova <olgnova@gmail.com>
2024-01-06 04:01:13 +0700

    2.3

--- 2.3/app.js
+++ 2.3/app.js
@@ -1,5 +1,8 @@
-const age = 27;
-console.log(typeof age);
+const titleProject = 'Оценка стоимости'
+const screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+const screenPrice = '20000'
+const percentage = '20'
+const fullPrice = '80000'
+const responsive = 'true'
 
-
-
+console.log(titleProject);
