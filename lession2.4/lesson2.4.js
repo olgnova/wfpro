ca2bbd30f6ced014578de1870adf227c34908fa2
olgnova <olgnova@gmail.com>
2024-01-09 03:08:51 +0700

    lession2.4

---
+++ lession2.4/lesson2.4.js
@@ -0,0 +1,8 @@
+let titleProject = 'Оценка стоимости'
+let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
+let screenPrice = '20000'
+let percentage = '20'
+let fullPrice = '80000'
+let responsive = 'true'
+
+console.log(titleProject);
