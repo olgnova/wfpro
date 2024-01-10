30357bc5c813dff1fffc26111bc14bbc1e0bf12b
olgnova <olgnova@gmail.com>
2024-01-11 02:32:27 +0700

    2-4-2

--- lession2.4/lesson2.4.js
+++ lession2.4/lesson2.4.js
@@ -21,23 +21,23 @@
 // console.log (res);
 
 
-let screenPrice = '20000'
-let percentage = '20'
-let titleProject = prompt ('Название проекта')
-let screensValue = prompt ('шаблонные, с уникальным дизайном, с анимациями')
-let responsive = prompt('Нужен ли респонсивный сайт?') === 'confirm';
-let service1 = prompt ('Какой сервис нужен?')
-let servicePrice1 = prompt ('Сколько это будет стоить?')
-let service2 = prompt ('Какой еще сервис тебе нужен?')
-let servicePrice2 = prompt ('Сколько будет стоить этот второй сервис?')
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
-let servicePercentPrice = fullPrice - (fullPrice * 0.15);
+let screenPrice = 20000;
+let percentage = 20;
+let titleProject = prompt ('Название проекта');
+let screensValue = prompt ('шаблонные, с уникальным дизайном, с анимациями');
+let responsive = confirm ('Нужен ли респонсивный сайт?');
+let service1 = prompt ('Какой сервис нужен?');
+let servicePrice1 = prompt ('Сколько это будет стоить?');
+let service2 = prompt ('Какой еще сервис тебе нужен?');
+let servicePrice2 = prompt ('Сколько будет стоить этот второй сервис?');
+let fullPrice = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2);
+let servicePercentPrice = parseInt(fullPrice) - (parseInt(fullPrice) * parseInt(percentage) / 100);
 console.log(servicePercentPrice);
-let roundedPrice = Math.round(servicePercentPrice);
+let roundedPrice = Math.floor(servicePercentPrice);
 console.log(roundedPrice);
-if (fullPrice > 50000) {
+if (fullPrice >= 50000) {
 console.log ('Сделаем скидку в 10%');
-} else if (fullPrice > 20000 && fullPrice < 40000) {
+} else if (fullPrice >= 20000 && fullPrice <= 40000) {
 console.log ('сделаем скидку 5%');
 } else if (fullPrice < 20000 && fullPrice > 0) {
 console.log ('Скидка не предусмотрена');
